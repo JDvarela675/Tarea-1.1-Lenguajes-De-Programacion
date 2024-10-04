62749184843457100c2a69c00bd05006f60ba831
JDvarela675 <jdvarelas@unah.hn>
2024-10-04 10:16:28 -0600

    se agrega el ejercicio 6

---
+++ tarea1.1_ejercicio6.dart
@@ -0,0 +1,21 @@
+//Crea una función que calcule el promedio de los elementos de una lista.
+
+void main(){
+List<double> listaP=[10,56.45,45,33,23.12];
+promedioLista(listaP);
+
+}
+
+void promedioLista ( List<double> lista){
+  if(lista.isEmpty){ //prevenir que la lista ingresada venga vacia 
+     print('La lista que usted ingreso esta vacia.');
+     return;
+  }
+ 
+   double suma=0;
+for(int i = 0; i < lista.length; i++){
+   suma+=lista[i];
+}
+double promedio=suma/lista.length;
+  print('El promedio de la lista $lista es:$promedio');
+}
